07d0f9ebb84d6ea98513b365f13aa4d1688b4c99
esotericpig <esotericpig@users.noreply.github.com>
2020-02-29 14:01:29 +0800

    Add App class for a basic CLI

--- lib/yard_ghurt.rb
+++ lib/yard_ghurt.rb
@@ -4,7 +4,7 @@
 
 #--
 # This file is part of YardGhurt.
-# Copyright (c) 2019 Jonathan Bradley Whited (@esotericpig)
+# Copyright (c) 2019-2020 Jonathan Bradley Whited (@esotericpig)
 # 
 # YardGhurt is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -21,11 +21,13 @@
 #++
 
 
+require 'optparse'
 require 'yard_ghurt/anchor_links'
 require 'yard_ghurt/gfm_fix_task'
 require 'yard_ghurt/ghp_sync_task'
 require 'yard_ghurt/util'
 require 'yard_ghurt/version'
+
 
 ###
 # YARDoc GitHub Rake Tasks
@@ -37,4 +39,56 @@ module YardGhurt
   # Internal code should use +Util.+!
   # See {Util} for details.
   include Util
+  
+  ###
+  # A simple CLI app used in file +bin/yard_ghurt+.
+  # 
+  # Mainly for getting GitHub/YARDoc anchor link IDs.
+  # 
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  1.2.0
+  ###
+  class App
+    attr_reader :args
+    
+    def initialize(args=ARGV)
+      super()
+      
+      @args = args
+    end
+    
+    def run()
+      parser = OptionParser.new() do |op|
+        op.program_name = 'yard_ghurt'
+        op.version = VERSION
+        
+        op.banner = "Usage: #{op.program_name} [options]"
+        
+        op.on('-g','--github <string>','Print GitHub anchor link ID of <string>') do |str|
+          al = AnchorLinks.new()
+          puts al.to_github_anchor_id(str)
+          exit
+        end
+        op.on('-y','--yard <string>','Print YARDoc anchor link ID of <string>') do |str|
+          al = AnchorLinks.new()
+          puts al.to_yard_anchor_id(str)
+          exit
+        end
+        
+        op.separator op.summary_indent + '---'
+        
+        op.on_tail('-h','--help','Print this help') do
+          puts op
+          exit
+        end
+        op.on_tail('-v','--version','Print the version') do
+          puts "#{op.program_name} v#{op.version}"
+          exit
+        end
+      end
+      
+      parser.parse!(@args)
+      puts parser # Print help if nothing was parsed
+    end
+  end
 end
